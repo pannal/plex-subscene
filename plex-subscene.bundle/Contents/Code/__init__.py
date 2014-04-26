39eb8dec92fb0968afacf3768c07c1affea0a7cf
kronenthaler <kronenthaler@gmail.com>
2014-04-26 18:59:11 +0200

    ADD correct download and register of metadata.

--- plex-subscene.bundle/Contents/Code/__init__.py
+++ plex-subscene.bundle/Contents/Code/__init__.py
@@ -1,13 +1,13 @@
 import string
 import os
 import urllib
+import tempfile
 import zipfile
 import re
 import copy
+import sys
 
-#Plex Theme Music
-#SUBSCENE_LANG_FILTER = "http://subscene.com/filter"
-SUBSCENE_LANG_FILTER = "http://localhost/data"
+SUBSCENE_LANG_FILTER = "http://subscene.com/filter"
 SUBSCENE_RETURN_URL = "/subtitles/release?%s"
 SUBSCENE_QUERY = "http://subscene.com/subtitles/release?%s"
 SUBSCENE_HOST = "http://subscene.com"
@@ -15,12 +15,7 @@ SUBSCENE_LANGUAGE_LOOKUP = {'Swedish': '39', 'German': '19', 'Farsi/Persian': '4
 
 
 def Start():
-    HTTP.CacheTime = CACHE_1DAY
-    filename = '/media/movies/Get.Low.2010.720p.BRRip.XviD.AC3-FLAWL3SS/Get.Louw.2010.720p.BRRip.XviD.AC3-FLAWL3SS.avi3'
-    #Log(distance('a','b'))
-    #Log(distance('zzzz','wwwwwwwwwww'))
-    #Log(distance('masage','message'))
-    Log(getSubsForPart(filename))
+    HTTP.CacheTime = 0
     Log("Subscene plugin started!!!")
     
     
@@ -32,7 +27,9 @@ class SubInfo():
         self.sub = sub
         self.name = name
         self.ext = string.split(self.name, '.')[-1]
-
+        
+    def __str__( self ):
+        return (self.lang, self.url, self.sub, self.name, self.ext).__str__()
 
 def distance(src, dst):
     src = src.strip().lower()
@@ -44,15 +41,15 @@ def distance(src, dst):
     sizet=len(src)+1
     matrix = [[0 for x in xrange(sizet)] for x in xrange(sizep)]
     
-    matrix[0][0]=0;
+    matrix[0][0] = 0
     for i in range(1, sizep):
         matrix[i][0] = i
 
-    for i in range(1, sizet):#(i=1;i<=sizet;matriz[0][i]=i++):
+    for i in range(1, sizet):
         matrix[0][i] = i
         
-    for i in range(1, sizep):#(i=1;i<=sizep;i++):
-        for j in range(1, sizet):#(j=1;j<=sizet;j++):
+    for i in range(1, sizep):
+        for j in range(1, sizet):
             if dst[i-1]==src[j-1]:
                 val = matrix[i-1][j-1]
             else:
@@ -83,10 +80,10 @@ def searchSubs(lang, filename):
     # for each subpage, analyze the proximity of the 2 strings!
     for subtitle in subpages:
         data = subtitle.xpath('div/span');
-        language = data[0].text
+        language = data[0].text.strip().lower()
         alternative = data[1].text
         
-        if language != lang:
+        if language != lang.strip().lower():
             continue
         
         score = distance(name, alternative)
@@ -112,20 +109,19 @@ def getSubsForPart(filename):
         root = HTML.ElementFromURL(SUBSCENE_HOST + url)
         downloadItem = root.xpath("//a[@id='downloadButton']/@href")[0]
         finalDownload = SUBSCENE_HOST + downloadItem
-        try:
-            zipArchive = Archive.ZipFromURL(finalDownload)
-
-            for name in zipArchive:
-                if name[-1] == "/":
-                    Log("Ignoring folder")
-                    continue
-
-                subData = zipArchive[name]
-                si = SubInfo(lang, finalDownload, subData, name)
-                siList.append(si)
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-            
+        
+        request = HTTP.Request(url=finalDownload, immediate=True)
+        request.load()
+        file = tempfile.NamedTemporaryFile(mode='w+b',delete=False)
+        file.write(request.content)
+        file.close()
+        
+        zipArchive = zipfile.ZipFile(file.name)
+        for subName in zipArchive.namelist():
+            subData = zipArchive.read(subName)
+            si = SubInfo(lang, finalDownload, subData, subName)
+            siList.append(si)
+                
     return siList
 
 # Agent classes
@@ -139,7 +135,15 @@ class PlexSubsceneAgentMovies(Agent.Movies):
         results.Append(MetadataSearchResult(id = media.primary_metadata.id, score = 100))
 
     def update(self, metadata, media, lang):
-        pass
+        for item in media.items:
+            for part in item.parts:
+                Log("Title: %s" % media.title)
+                Log("Filename: %s" % part.file)
+                siList = getSubsForPart(part.file)
+
+                for si in siList:
+                    part.subtitles[Locale.Language.Match(si.lang)][si.url] = Proxy.Media(si.sub, ext=si.ext)
+
 
 
 class PlexSubsceneAgentTVShows(Agent.TV_Shows):
@@ -152,4 +156,14 @@ class PlexSubsceneAgentTVShows(Agent.TV_Shows):
         results.Append(MetadataSearchResult(id = media.primary_metadata.id, score = 100))
 
     def update(self, metadata, media, lang):
-        pass
+        for season in media.seasons:
+            for episode in media.seasons[season].episodes:
+                for item in media.seasons[season].episodes[episode].items:
+                    Log("show: %s" % media.title)
+                    Log("Season: %s, Ep: %s" % (season, episode))
+                    for part in item.parts:
+                        siList = getSubsForPart(part.file)
+
+                        for si in siList:
+                            part.subtitles[Locale.Language.Match(si.lang)][si.url] = Proxy.Media(si.sub, ext=si.ext)
+
